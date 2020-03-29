3ac3c9ae011664d6d1cbc7cb13461442e0946dce
Chris <weeblytricks@gmail.com>
2020-03-29 11:04:21 -0400

    initial upload

---
+++ js/custom.js
@@ -0,0 +1,40 @@
+jQuery(function($) {
+
+  // Sticky Sidebar
+  $('.themeProductContent, .themeProductStats').theiaStickySidebar({
+    additionalMarginTop: 100,
+	additionalMarginBottom: 20
+  });
+  
+  // Sticky Sidebar Bottom Content Animation  
+  var $header = $('.themeProductStats');
+  
+  $(window).scroll(function () { 
+    if ($header.offset().top - $(this).scrollTop() >= 100) {
+      $(".themeProductStickBottom").removeClass('show');
+    } 
+	else if ($header.offset().top - $(this).scrollTop() < 100) {
+      $(".themeProductStickBottom").addClass('show');
+    }
+  });
+  
+  // Scroll To Top Button
+  $(window).scroll(function() {    
+    var scroll = $(window).scrollTop();
+    if (scroll >= 1) {
+      $(".scroll").removeClass("animated zoomOut");
+      $(".scroll").css('display', 'flex');
+      $(".scroll").addClass("animated zoomIn");
+    } 
+	else {
+      $(".scroll").removeClass("animated zoomIn");
+      $(".scroll").addClass("animated zoomOut");
+    }
+  });
+
+  $(".scroll").click(function() {
+    $("html, body").animate({ scrollTop: 0 }, "slow");
+    return false;
+  });
+	
+});
