59976c42adb12ef7cee1b9326e574af3172cad70
Chris <weeblytricks@gmail.com>
2020-07-10 17:56:25 -0400

    changes

--- js/custom.js
+++ js/custom.js
@@ -3,7 +3,9 @@ jQuery(function($) {
   function isScrolledIntoView(elem) {
     var docViewTop = $(window).scrollTop();
     var docViewBottom = docViewTop + $(window).height();
+
     var elemTop = $(elem).offset().top;
+    var elemBottom = elemTop + $(elem).height();
 	return ((elemBottom <= docViewBottom) && (elemTop >= docViewTop));
   }
 
